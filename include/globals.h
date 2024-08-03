2f5174ceee0a9ed589e14a6c019255c3885ebb98
aryanthepain <aryan.gupta@iitg.ac.in>
2024-08-03 19:59:26 +0530

    keybinds

--- include/globals.h
+++
@@ -1,9 +0,0 @@
-#include <SDL2/SDL.h>
-
-extern SDL_Window* gWindow;
-extern SDL_Surface* gScreenSurface;
-extern SDL_Surface* gHelloWorld;
-
-//screen dimension constants
-const int SCREEN_WIDTH=640;
-const int SCREEN_HEIGHT=480;
