b92b1214f3e90139a45126803c0b05edc6663bdc
aryanthepain <aryan.gupta@iitg.ac.in>
2024-08-24 00:03:48 +0530

    prettieer part1

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,35 +1,37 @@
-// author:Aryanthepain
+// author: Aryanthepain
 #include "Main.h"
 
-void init_game(Snake &snake1, Snake &snake2, int &food_x, int &food_y, int max_x, int max_y, WINDOW* win)
+void init_game(Snake &snake1, Snake &snake2, int &food_x, int &food_y, int max_x, int max_y, WINDOW *win)
 {
 	// Initialize food position
-	food_x=max_x/2;
-	food_y=max_y/2;
-	
-	//initialise snake 1
-/*	snake1.body.pb(mp(12,1));
-	snake1.body.pb(mp(11,1));
-	snake1.body.pb(mp(10,1));
-	snake1.body.pb(mp(9,1));
-	snake1.body.pb(mp(8,1));
-	snake1.body.pb(mp(7,1));
-	snake1.body.pb(mp(6,1));
-	snake1.body.pb(mp(5,1));
-	snake1.body.pb(mp(4,1));
-*/	snake1.body.pb(mp(3,1));
-	snake1.body.pb(mp(2,1));
-	snake1.body.pb(mp(1,1));
-	snake1.direction=RIGHT;
-	snake1.score=0;
-	
-	//initialise snake 2
-	snake2.body.pb(mp(max_x-5,max_y-3));
-	snake2.body.pb(mp(max_x-4,max_y-3));
-	snake2.body.pb(mp(max_x-3,max_y-3));
-	snake2.direction=LEFT;
-	snake2.score=0;
-	
+	food_x = max_x / 2;
+	food_y = max_y / 2;
+
+	// initialise snake 1
+	// debug code
+	/*	snake1.body.pb(mp(12,1));
+		snake1.body.pb(mp(11,1));
+		snake1.body.pb(mp(10,1));
+		snake1.body.pb(mp(9,1));
+		snake1.body.pb(mp(8,1));
+		snake1.body.pb(mp(7,1));
+		snake1.body.pb(mp(6,1));
+		snake1.body.pb(mp(5,1));
+		snake1.body.pb(mp(4,1));
+	*/
+	snake1.body.pb(mp(3, 1));
+	snake1.body.pb(mp(2, 1));
+	snake1.body.pb(mp(1, 1));
+	snake1.direction = RIGHT;
+	snake1.score = 0;
+
+	// initialise snake 2
+	snake2.body.pb(mp(max_x - 5, max_y - 3));
+	snake2.body.pb(mp(max_x - 4, max_y - 3));
+	snake2.body.pb(mp(max_x - 3, max_y - 3));
+	snake2.direction = LEFT;
+	snake2.score = 0;
+
 	draw(snake1, snake2, food_x, food_y, win);
 }
 
@@ -37,74 +39,87 @@ int main()
 {
 	// Initialize ncurses
 	initscr();
-	if (stdscr == nullptr) {
-        cerr << "Error initializing ncurses." << endl;
-        return 1;
-    }
-	cbreak();
-	noecho();
-	keypad(stdscr, TRUE);
-	curs_set(0);
-//	timeout(100);
+	if (stdscr == nullptr)
+	{
+		cerr << "Error initializing ncurses." << endl;
+		return 1;
+	}
+	cbreak();			  // makes ncurses work for a single character
+	noecho();			  // inout does not display on terminal
+	keypad(stdscr, TRUE); // special keys are able to be input
+	curs_set(0);		  // removes cursor
+	//	timeout(100); //waits for getch() for 100ms, otherwise outputs ERR
 
 	int start_x, start_y;
 	getmaxyx(stdscr, start_y, start_x);
-	WINDOW* menu=newwin(start_y,start_x,0,0);
-	if (menu == nullptr) {
-        endwin();
-        cerr << "Error creating menu window." << endl;
-        return 1;
-    }
-	refresh();
-	MODE mode;
-	mode=display_menu(menu);
-/*	if(mode==single){
-		mvwprintw(stdscr, 0,0, "single");
+
+	// menu screen
+	WINDOW *menu = newwin(start_y, start_x, 0, 0);
+	if (menu == nullptr)
+	{
+		endwin();
+		cerr << "Error creating menu window." << endl;
+		return 1;
 	}
 	refresh();
-	getch();	
-*/
-	WINDOW* win=newwin(min(30,2*start_y/3), min(100, 2*start_x/3),1,0);
+	MODE mode;
+	mode = display_menu(menu);
+	/*	//debug code
+	if(mode==single){
+			mvwprintw(stdscr, 0,0, "single");
+		}
+		refresh();
+		getch();
+	*/
+	WINDOW *win = newwin(min(30, 2 * start_y / 3), min(100, 2 * start_x / 3), 1, 0);
 	int max_x, max_y;
 	getmaxyx(win, max_y, max_x);
-	mvwprintw(stdscr, 0, (max_x-26)/2,"SNAKES GAME BY ARYAN GUPTA");
+
+	mvwprintw(stdscr, 0, (max_x - 26) / 2, "SNAKES GAME BY ARYAN GUPTA");
 	refresh();
-	box(win,0,0);
+	box(win, 0, 0);
 	wrefresh(win);
 
 	Snake snake1, snake2;
 	int food_x, food_y;
 	init_game(snake1, snake2, food_x, food_y, max_x, max_y, win);
-	bool food_present=true;
-	
-	timeout(100);
-	while (true) {
+	bool food_present = true;
+
+	timeout(100); // getch set to time out after 100ms
+	while (true)
+	{
 		handle_input(snake1, snake2);
-		
-		//remove tail piece
+
+		// remove tail piece
 		mvwprintw(win, snake1.body.back().second, snake1.body.back().first, " ");
 		mvwprintw(win, snake2.body.back().second, snake2.body.back().first, " ");
-		
-		//update food and snakes
-		food_present=(update_snake(snake1, max_x, max_y, food_x, food_y) && update_snake(snake2, max_x, max_y, food_x, food_y));
+
+		// update food and snakes
+		food_present = (update_snake(snake1, max_x, max_y, food_x, food_y) && update_snake(snake2, max_x, max_y, food_x, food_y));
 		update_food(food_present, snake1, snake2, food_x, food_y, max_x, max_y);
-		
+
 		draw(snake1, snake2, food_x, food_y, win);
 		wrefresh(win);
 
 		// Check for collisions
-		if (check_collision(snake1, snake2)) {
-		    break; // End the game if a collision occurs
+		if (check_collision(snake1, snake2))
+		{
+			break; // End the game if a collision occurs
 		}
 	}
 
-//quit screen
-//different win conditions
-	mvwprintw(win, 3, 3, "hello");
+	// quit screen
+	// different win conditions
+	mvwprintw(win, max_y / 2, max_x / 2, "GAME OVER!");
 	wrefresh(win);
+
+	// delay after game ends to prevent accidental clicks
 	napms(1000);
+	mvwprintw(win, max_y / 2 + 2, max_x / 2, "Press any key to continue");
+	wrefresh(win);
 	timeout(-1);
 	getch();
+
 	// Cleanup
 	endwin();
 	return 0;
--- src/menu.cpp
+++ src/menu.cpp
@@ -1,84 +1,102 @@
+// author: Aryanthepain
 #include "menu.h"
 
-void handle_input(WINDOW* menu, MODE &mode, bool &confirm){
-	int ch=wgetch(menu);
-	
-	switch(ch){
-		case 65: mode=single; break;
-		case 66: mode=multi; break;	
-		case 'e': confirm=true; break;
-		default: break;
+void handle_input(WINDOW *menu, MODE &mode, bool &confirm)
+{
+	int ch = wgetch(menu);
+
+	switch (ch)
+	{
+	case KEY_UP:
+		mode = single;
+		break;
+	case KEY_DOWN:
+		mode = multi;
+		break;
+	case 'e':
+		confirm = true;
+		break;
+	default:
+		break;
 	}
 }
 
-void print_mode(WINDOW* menu, MODE mode){
-    mvwprintw(menu, 6, 0, " ");
-    mvwprintw(menu, 7, 0, " ");
-    
-    if(mode == single){
-        mvwprintw(menu, 6, 0, ">");
-    }
-    else{
-        mvwprintw(menu, 7, 0, ">");
-    }
-    wrefresh(menu);
+void print_mode(WINDOW *menu, MODE mode)
+{
+	mvwprintw(menu, 6, 0, " ");
+	mvwprintw(menu, 7, 0, " ");
+
+	if (mode == single)
+	{
+		mvwprintw(menu, 6, 0, ">");
+	}
+	else
+	{
+		mvwprintw(menu, 7, 0, ">");
+	}
+	wrefresh(menu);
 }
 
-void tutorial(WINDOW* menu, MODE mode){
+void tutorial(WINDOW *menu, MODE mode)
+{
 	wclear(menu);
-	
-	if(mode==single){
-		mvwprintw(menu, 0,0, "WASD to move");
-		mvwprintw(menu, 1,0, "eat 'A' to increase score");
-		mvwprintw(menu, 2,0, "Do NOT eat yourself");
+
+	if (mode == single)
+	{
+		mvwprintw(menu, 0, 0, "WASD to move");
+		mvwprintw(menu, 1, 0, "eat 'A' to increase score");
+		mvwprintw(menu, 2, 0, "Do NOT eat yourself");
 	}
-	else{
-		mvwprintw(menu, 0,0, "eat 'A' to increase score");
-		mvwprintw(menu, 1,0, "Do NOT eat yourself");
-		mvwprintw(menu, 2,0, "Do NOT eat other player");
-		mvwprintw(menu, 3,0, "Eliminate other player to win");
-		
-		mvwprintw(menu, 5,0, "Player 1 (O)");
-		mvwprintw(menu, 6,0, "WASD to move");
-		
-		mvwprintw(menu, 8,0, "Player 2 (H)");
-		mvwprintw(menu, 9,0, "arrow keys to move");
+	else
+	{
+		mvwprintw(menu, 0, 0, "eat 'A' to increase score");
+		mvwprintw(menu, 1, 0, "Do NOT eat yourself");
+		mvwprintw(menu, 2, 0, "Do NOT eat other player");
+		mvwprintw(menu, 3, 0, "Eliminate other player to win");
+
+		mvwprintw(menu, 5, 0, "Player 1 (O)");
+		mvwprintw(menu, 6, 0, "WASD to move");
+
+		mvwprintw(menu, 8, 0, "Player 2 (H)");
+		mvwprintw(menu, 9, 0, "arrow keys to move");
 	}
-	
+
 	wrefresh(menu);
 }
 
-MODE display_menu(WINDOW* menu){
-	MODE mode=single;
-	
-	mvwprintw(menu, 0,0, "SNAKES GAME BY ARYAN GUPTA");
-	mvwprintw(menu, 2,0, "Make terminal full screen for best experience");
-	mvwprintw(menu, 4,0, "Select game mode:('e' to confirm)");
-	mvwprintw(menu, 6,2, "Single Player");
-	mvwprintw(menu, 7,2, "Two Player");
-	
+MODE display_menu(WINDOW *menu)
+{
+	MODE mode = single;
+
+	mvwprintw(menu, 0, 0, "SNAKES GAME BY ARYAN GUPTA");
+	mvwprintw(menu, 2, 0, "Make terminal full screen for best experience");
+	mvwprintw(menu, 4, 0, "Select game mode:('e' to confirm)");
+	mvwprintw(menu, 6, 2, "Single Player");
+	mvwprintw(menu, 7, 2, "Two Player");
+
 	wrefresh(menu);
-	
-	bool confirm=false;
-	while(!confirm){
+
+	bool confirm = false;
+	while (!confirm)
+	{
 		print_mode(menu, mode);
-		
+
 		handle_input(menu, mode, confirm);
 	}
-	
+
 	wclear(menu);
 	wrefresh(menu);
-	
+
 	tutorial(menu, mode);
-	
+
 	napms(1000);
-	mvwprintw(menu, 12,0, "Press any key to continue");
+	mvwprintw(menu, 12, 0, "Press any key to continue");
 	wrefresh(menu);
-	
+
 	getch();
 	werase(menu);
 	wrefresh(menu);
 	delwin(menu);
-	
+
 	return mode;
 }
